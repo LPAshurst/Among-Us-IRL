8d28c0a68af682a97c6807fc6e2d1ebb380d0ec4
Zane Metz <metzza@dukes.jmu.edu>
2024-07-28 23:31:07 -0400

    Add endpoint to get tasks for a user

--- backend/src/routes/task.route.ts
+++ backend/src/routes/task.route.ts
@@ -3,5 +3,13 @@ import taskController from '../controller/task.controller';
 const taskRouter = Router();
 // specifies the endpoint and the method to call
 taskRouter.get('/', taskController.getAll);
+
+taskRouter.get('/', function(req, res){
+    res.send('id: ' + req.query.id);
+  });
+taskRouter.get('/:id', (req, res) => {
+    taskController.getUserTasks(req, res, +req.params.id);
+}
+);
 // export the router
 export default taskRouter;
