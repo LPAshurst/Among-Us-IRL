c6aeea9613ef74975b8d30fcba8f3d399297341d
LPAshurst <LPAshurst@hotmail.com>
2024-07-28 23:27:24 -0400

    Added some extra swiper stuff

--- amongus-frontend/src/App.tsx
+++ amongus-frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import { Autoplay} from 'swiper/modules';
 import './App.css';
 import AmongUsLogo from './ui/amongus_logo';
 import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import red_among from './assets/red_among.png';
+
+import 'swiper/css';
+import 'swiper/css/autoplay'
+
 
 export default function App() {
   return (
@@ -9,11 +16,41 @@ export default function App() {
         <AmongUsLogo />
       </div>
 
-      <Link to="task-screen" className='w-40 h-50 self-center mt-40'>
-        <button className='bg-blue-900'>
-          <span className='text-[25px] font-lusitana text-neutral-200'> Log in </span>
-        </button>
-      </Link>
+      <div className='flex w-full h-32 justify-center'>
+        <Link to="task-screen" className='self-end'>
+          <button className='bg-blue-900'>
+            <span className='text-[20px] font-lusitana text-neutral-200'> Create A Room </span>
+          </button>
+        </Link>
+      </div>
+
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={6}
+        centeredSlides={true}
+        slidesPerView={1.5}
+        loop={true}
+
+        autoplay={
+          {
+            delay: 4500,
+            disableOnInteraction: false,
+            
+          }
+        }
+        className='w-full h-32 mt-10'
+      >
+        
+        <SwiperSlide><img src={red_among} width={100} height={100}/></SwiperSlide>
+        <SwiperSlide><img src={red_among} width={100} height={100}/></SwiperSlide>
+        <SwiperSlide><img src={red_among} width={100} height={100}/></SwiperSlide>
+        <SwiperSlide><img src={red_among} width={100} height={100}/></SwiperSlide>
+        <SwiperSlide><img src={red_among} width={100} height={100}/></SwiperSlide>
+
+      
+      </Swiper>
+
+      
       
     </>
   );
