77cf48617b8a98c3a2f79e9c034bcd0b9748e4a6
Zane Metz <metzza@dukes.jmu.edu>
2024-07-28 22:53:17 -0400

    typo in db init

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -11,7 +11,7 @@ CREATE TABLE `user` (
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY(id),
   UNIQUE(username),
-  INDEX name_idn (username)
+  INDEX name_ind (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
