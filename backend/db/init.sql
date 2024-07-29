ed18b36a79e141ef28e189831d7c322a3a7413f8
Zane Metz <metzza@dukes.jmu.edu>
2024-07-28 22:49:58 -0400

    add user table to db

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -2,52 +2,48 @@
 --
 -- Host: localhost    Database: amongus
 -- ------------------------------------------------------
--- Server version	8.0.39
+use amongus;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (  
+  `id` int AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY(id),
+  UNIQUE(username),
+  INDEX name_idn (username)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `task`
---
 
 DROP TABLE IF EXISTS `task`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `task` (
+  `id` int AUTO_INCREMENT,
+  `user_id` int NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `location` varchar(255) DEFAULT NULL,
-  `difficulty` varchar(255) DEFAULT NULL
+  `difficulty` varchar(255) DEFAULT NULL,
+  PRIMARY KEY(id),
+  INDEX u_ind (user_id),
+  FOREIGN KEY (user_id)
+      REFERENCES user(id)
+      ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO task VALUES ('Manage Wiring','Kitchen','Hard');
+
 
 --
 -- Dumping data for table `task`
 --
+-- LOCK TABLES `task` WRITE;
+-- /*!40000 ALTER TABLE `task` DISABLE KEYS */;
+-- /*!40000 ALTER TABLE `task` ENABLE KEYS */;
+-- UNLOCK TABLES;
 
-LOCK TABLES `task` WRITE;
-/*!40000 ALTER TABLE `task` DISABLE KEYS */;
-/*!40000 ALTER TABLE `task` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-26  2:02:12
-use amongus;
+
+INSERT INTO user VALUES (1, "Admin Zane", "donotcopy");
+INSERT INTO user VALUES (2, "Admin Lorenzo", "stupidhead");
+
+INSERT INTO task (user_id, name, location, difficulty) VALUES (1, 'Manage Wiring','Kitchen','Hard');
+-- SELECT * FROM task INNER JOIN user ON task.user_id = user.id WHERE user.id = 1;
