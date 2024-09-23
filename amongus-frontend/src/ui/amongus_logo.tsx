67d53c08d65aefeb54012e972515cb77dfa91518
LPAshurst <LPAshurst@hotmail.com>
2024-09-23 18:13:28 -0400

    exxtra stuff

--- amongus-frontend/src/ui/amongus_logo.tsx
+++ amongus-frontend/src/ui/amongus_logo.tsx
@@ -5,15 +5,17 @@ import red_among from '../assets/red_among.png';
 export default function AmongUsLogo() {
   return (
     <div
-      className='flex flex-row justify-center items-center'
+      className='flex'
     >
-      <Link to="/"><img
+      <Link to="/">
+      <img
         src={red_among}
         width={45}
       />
       </Link>
 
-      <p className="text-[25px] font-lusitana text-neutral-200 p-2.5">Amog us in real life</p>
+      <p className="text-[25px] font-lusitana text-neutral-200 p-2.5 self-end">Amog us in real life</p>
+      
     </div>
   );
 }
