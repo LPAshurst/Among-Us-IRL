39887d982a3ee587e7925baac0f24c2497a3f096
LPAshurst <LPAshurst@hotmail.com>
2024-10-05 22:29:26 -0400

    some changes to the types in game create as well as updates in the sql database to reflect this

--- amongus-frontend/src/routes/game-create.tsx
+++ amongus-frontend/src/routes/game-create.tsx
@@ -1,30 +1,29 @@
-import { Box, Stack, Typography, Button, Table, TableContainer, Paper, TableBody, TableRow, TableCell, List, ListItemText, TableHead, IconButton } from "@mui/material";
+import { Box, Stack, Typography, Button, Table, TableContainer, Paper, TableBody, TableRow, TableCell, List, ListItemText, TableHead, IconButton, Fab } from "@mui/material";
 import ClearIcon from '@mui/icons-material/Clear';
 import { useEffect, useState } from "react";
 import Navbar from "../ui/navbar";
 import { socket } from "../socket";
-
+import { Task } from "../types/GameTypes";
+import AddIcon from '@mui/icons-material/Add';
 
 export default function CreationPage() {
 
-  const [tasklist, setTasklist] = useState();
+  const [tasklist, setTasklist] = useState<Task[]>();
   const [players, setPlayers] = useState([]); //Remove default names later
-
 
   useEffect(() => {
     fetch("http://localhost:3010/api/tasks/list", {
       method: "GET",
       mode: "cors"
     }).then(response => response.json()).then(value => {
-      setTasklist(value.result);
-      console.log(value.result)
+      setTasklist(value.result.entries);
+      console.log(value.result);
     }).catch(err => {
       console.error(err);
     });
-
-    socket.emit("join", "room");
-
     
+    socket.emit("join", "room");
+  
   }, [])
 
   socket.on("clientList", (clientList) => {
@@ -33,6 +32,12 @@ export default function CreationPage() {
 
   });
 
+  function removeTask() {
+
+    console.log("XD")
+
+
+  }
 
   function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
@@ -40,6 +45,7 @@ export default function CreationPage() {
 
   function TaskContainer() {
     return (
+
       <TableContainer component={Paper}>
         <Table sx={{ padding: 2, minWidth: 500 }}>
           <TableHead>
@@ -51,47 +57,24 @@ export default function CreationPage() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {tasklist?.entries.map((task) => (
+            {tasklist?.map((task) => (
               <TableRow key={task.id}>
-                <TableCell>{task.name}</TableCell>
+                <TableCell>{task.title}</TableCell>
                 <TableCell>{task.description}</TableCell>
                 <TableCell>{task.location}</TableCell>
-                <TableCell><IconButton aria-label="delete" variant="remove" size="small"><ClearIcon sx={{minWidth: 20, width: 20, minHeight: 20, height: 20,}} /></IconButton></TableCell>
+                <TableCell>
+                  <IconButton variant="remove" aria-label="delete" size="small" onClick={removeTask}>
+                    <ClearIcon sx={{minWidth: 20, width: 20, minHeight: 20, height: 20}} />
+                  </IconButton>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-    );
-    return (
-      <Stack spacing={1.5}>
-        <Stack direction="row" sx={{ alignItems: 'center' }} display="flex" gap={2}>
-          <Typography color="white">Tasks</Typography>
-          <Box justifySelf="end" display="flex" gap={2}>
-            <Button variant="outlined" color='secondary'>Load</Button>
-            <Button variant="outlined" color='secondary'>Saves</Button>
-          </Box>
-        </Stack>
-        <Stack sx={{ p: 2, border: '1px solid white' }} spacing={0}>
-          <Stack sx={{ border: '1px solid white', color: 'white' }} direction="row">
-            <Box sx={{ border: '1px solid white', padding: 2, width: '15vw' }}>Name</Box>
-            <Box sx={{ border: '1px solid white', padding: 2, width: '15vw' }}>Location</Box>
-            <Box sx={{ border: '1px solid white', padding: 2, width: '15vw' }}>Difficulty</Box>
-          </Stack>
-          {tasklist?.entries.map(item => (
-            <Task name={item.name} location={item.location} difficulty={item.difficulty}></Task>
-          ))}
-        </Stack>
-      </Stack>
-    );
-  }
 
-  function Task(props: any) {
-    return (<Stack sx={{ border: '1px solid white', color: 'white' }} direction="row">
-      <Box sx={{ border: '1px solid white', padding: 2, width: '15vw' }}>{props.name}</Box>
-      <Box sx={{ border: '1px solid white', padding: 2, width: '15vw' }}>{props.location}</Box>
-      <Box sx={{ border: '1px solid white', padding: 2, width: '15vw' }}>{props.difficulty}</Box>
-    </Stack>);
+
+    );
   }
 
   const gameCode = getRandomInt(50000);
@@ -109,28 +92,18 @@ export default function CreationPage() {
             <Stack spacing={20} direction={"row"}>
               <TaskContainer />
             </Stack>
-            <Button variant="outlined" color='secondary'>Start</Button>
+            <Box justifyContent="space-around" alignItems="center" display="flex" gap={2} width={"100%"}>
+              <Button variant="outlined" color='secondary' sx={{ marginLeft: "auto", marginRight: "auto" }}>Start</Button>
+              <Fab size="small" color="secondary" aria-label="add" sx={{alignSelf:"flex-end"}}>
+                <AddIcon />
+              </Fab>
+            </Box>
           </Box>
           <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
             <Box alignItems={"center"} display="flex" flexDirection="column">
               <Typography color="white">Players</Typography>
               <Typography color="white">({players.length}/10)</Typography>
             </Box>
-            {/* <Box sx={{ border: '1px solid white', height: '40vh', width: '25vh', padding: 2 }}>
-              <Typography color="white">Jackson</Typography>
-              <Typography color="white">Lorenzo</Typography>
-              <Typography color="white">Zane</Typography>
-            </Box> */}
-            {/* <TableContainer component={Paper}>
-              <Table sx={{ border: '1px solid white', width: '25vh', padding: 2 }}>
-                <TableBody>
-                  {players.map((player) => (
-                    <TableRow key={player}><TableCell>{player}</TableCell></TableRow>
-                  ))}
-                  
-                </TableBody>
-              </Table>
-            </TableContainer> */}
             <List sx={{ border: '1px solid white', width: '25vh', padding: 2 }} component={Paper} elevation={0}>
               {players.map((player) => (
                 <Paper key={player} elevation={1}><ListItemText><Typography fontFamily={"Comfortaa"}>{player}</Typography></ListItemText></Paper>
--- backend/db/init.sql
+++ backend/db/init.sql
@@ -30,7 +30,7 @@ DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
   `id` int AUTO_INCREMENT,
   `list_id` int NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
   `location` varchar(255) DEFAULT NULL,
   `difficulty` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
@@ -58,10 +58,10 @@ INSERT INTO user VALUES (1, "Admin Zane", "donotcopy");
 INSERT INTO user VALUES (2, "Admin Lorenzo", "stupidhead");
 
 INSERT INTO tasklist (id, user_id, name) VALUES (1, 1, 'Example List 1');
-INSERT INTO task (list_id, name, location, difficulty, description) VALUES (1, 'Manage Wiring','Kitchen','Hard', 'Go to the kitchen and manage the wiring on the counter.');
-INSERT INTO task (list_id, name, location, difficulty, description) VALUES (1, 'Task 1','Location 1','Easy', 'This is an example task.');
-INSERT INTO task (list_id, name, location, difficulty, description) VALUES (1, 'Task 2','Location 2','Medium', 'This is an example task.');
-INSERT INTO task (list_id, name, location, difficulty, description) VALUES (1, 'Task 3','Location 3','Hard', 'This is an example task.');
-INSERT INTO task (list_id, name, location, difficulty, description) VALUES (1, 'Task 4','Location 4','Easy', 'This is an example task.');
-INSERT INTO task (list_id, name, location, difficulty, description) VALUES (1, 'Task 5','Location 5','Medium', 'This is an example task.');
+INSERT INTO task (list_id, title, location, difficulty, description) VALUES (1, 'Manage Wiring','Kitchen','Hard', 'Go to the kitchen and manage the wiring on the counter.');
+INSERT INTO task (list_id, title, location, difficulty, description) VALUES (1, 'Task 1','Location 1','Easy', 'This is an example task.');
+INSERT INTO task (list_id, title, location, difficulty, description) VALUES (1, 'Task 2','Location 2','Medium', 'This is an example task.');
+INSERT INTO task (list_id, title, location, difficulty, description) VALUES (1, 'Task 3','Location 3','Hard', 'This is an example task.');
+INSERT INTO task (list_id, title, location, difficulty, description) VALUES (1, 'Task 4','Location 4','Easy', 'This is an example task.');
+INSERT INTO task (list_id, title, location, difficulty, description) VALUES (1, 'Task 5','Location 5','Medium', 'This is an example task.');
 -- SELECT * FROM task INNER JOIN user ON task.user_id = user.id WHERE user.id = 1;
