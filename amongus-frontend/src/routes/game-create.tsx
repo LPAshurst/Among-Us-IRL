082629bbce18ea5c92fdd9486004365caae0e31a
LPAshurst <LPAshurst@hotmail.com>
2024-10-06 12:07:57 -0400

    new branch

--- amongus-frontend/src/routes/game-create.tsx
+++ amongus-frontend/src/routes/game-create.tsx
@@ -1,22 +1,41 @@
-import { Box, Stack, Typography, Button, Table, TableContainer, Paper, TableBody, TableRow, TableCell, List, ListItemText, TableHead, IconButton, Fab } from "@mui/material";
-import ClearIcon from '@mui/icons-material/Clear';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import {
+  GridRowsProp,
+  GridRowModesModel,
+  GridRowModes,
+  DataGrid,
+  GridColDef,
+  GridToolbarContainer,
+  GridActionsCellItem,
+  GridEventListener,
+  GridRowId,
+  GridRowModel,
+  GridRowEditStopReasons,
+  GridSlots,
+} from '@mui/x-data-grid';
 import { useEffect, useState } from "react";
-import Navbar from "../ui/navbar";
 import { socket } from "../socket";
 import { Task } from "../types/GameTypes";
-import AddIcon from '@mui/icons-material/Add';
-
+import { Paper } from '@mui/material';
 export default function CreationPage() {
 
-  const [tasklist, setTasklist] = useState<Task[]>();
+  const [rows, setRows] = useState<Task[]>();
   const [players, setPlayers] = useState([]); //Remove default names later
+  const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
 
   useEffect(() => {
     fetch("http://localhost:3010/api/tasks/list", {
       method: "GET",
       mode: "cors"
     }).then(response => response.json()).then(value => {
-      setTasklist(value.result.entries);
+      setRows(value.result.entries);
       console.log(value.result);
     }).catch(err => {
       console.error(err);
@@ -32,48 +51,174 @@ export default function CreationPage() {
 
   });
 
-  function removeTask() {
-
-    console.log("XD")
-
-
-  }
-
   function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
   }
 
+interface EditToolbarProps {
+  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+  setRowModesModel: (
+    newModel: (oldModel: GridRowModesModel) => GridRowModesModel,
+  ) => void;
+}
+
+function EditToolbar(props: EditToolbarProps) {
+  const { setRows, setRowModesModel } = props;
+
+  const handleClick = () => {
+    const id = randomId();
+    setRows((oldRows) => [
+      ...oldRows,
+      { id, name: '', age: '', role: '', isNew: true },
+    ]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+    }));
+  };
+
+  return (
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Add Task
+      </Button>
+    </GridToolbarContainer>
+  );
+}
+  const handleRowEditStop: GridEventListener<'rowEditStop'> = (params, event) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    setRows(rows?.filter((row) => row.id !== id));
+  };
+
+  const handleCancelClick = (id: GridRowId) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow!.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
+    }
+  };
+
+  const processRowUpdate = (newRow: GridRowModel) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Name', width: 180, editable: true },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 80,
+      align: 'left',
+      headerAlign: 'left',
+      editable: true,
+    },
+    {
+      field: 'joinDate',
+      headerName: 'Join date',
+      type: 'date',
+      width: 180,
+      editable: true,
+    },
+    {
+      field: 'role',
+      headerName: 'Department',
+      width: 220,
+      editable: true,
+      type: 'singleSelect',
+      valueOptions: ['Market', 'Finance', 'Development'],
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: 'primary.main',
+              }}
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
+
   function TaskContainer() {
     return (
 
-      <TableContainer component={Paper}>
-        <Table sx={{ padding: 2, minWidth: 500 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell variant="head">Name</TableCell>
-              <TableCell variant="head">Description</TableCell>
-              <TableCell variant="head">Location</TableCell>
-              <TableCell variant="head"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {tasklist?.map((task) => (
-              <TableRow key={task.id}>
-                <TableCell>{task.title}</TableCell>
-                <TableCell>{task.description}</TableCell>
-                <TableCell>{task.location}</TableCell>
-                <TableCell>
-                  <IconButton variant="remove" aria-label="delete" size="small" onClick={removeTask}>
-                    <ClearIcon sx={{minWidth: 20, width: 20, minHeight: 20, height: 20}} />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-
+      <DataGrid 
+        rows={rows} 
+        columns={columns}
+        editMode='row' 
+        rowModesModel={rowModesModel} 
+        onRowModesModelChange={handleRowModesModelChange} 
+        onRowEditStop={handleRowEditStop} 
+        processRowUpdate={processRowUpdate}
+        slots={{
+          toolbar: EditToolbar as GridSlots['toolbar']
+        }}
+        slotProps={{
+          toolbar: {setRows, setRowModesModel}
+        }}  > 
+      </DataGrid>
+      
     );
   }
 
